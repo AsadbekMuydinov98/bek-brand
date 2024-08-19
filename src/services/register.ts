823ee90aede93a1451cb4b49be1e01dc30930181
Asadbek <asasedmor@gmail.com>
2024-08-19 11:56:10 +0900

    all

--- src/services/register.ts
+++
@@ -1,7 +0,0 @@
-import { service } from ".";
-
-export default {
-  Login: (user) => service.post('/auth/login', user),
-  Signup: (user) => service.post('auth/register', user),
-  Profile: () => service.get('/auth/profile')
-}
